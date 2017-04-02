782673e1cfdfafa73d20fdcb57f22e933e96639e
Dimitry Polivaev <dpolivaev@gmx.de>
2017-04-02 11:26:24 +0200

    initial import

---
+++ src/main/java/org/freeplane/securegroovy/GroovyPatcher.java
@@ -0,0 +1,37 @@
+package org.freeplane.securegroovy;
+
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.dynamic.ClassFileLocator;
+import net.bytebuddy.implementation.MethodDelegation;
+import net.bytebuddy.matcher.ElementMatchers;
+import net.bytebuddy.pool.TypePool;
+
+public class GroovyPatcher {
+	private static final String CACHED_FIELD_CLASS = "org.codehaus.groovy.reflection.CachedField";
+	private static final String CACHED_METHOD_CLASS = "org.codehaus.groovy.reflection.CachedMethod";
+	
+	public static void apply(final ClassLoader pachedClassLoader){
+		final ByteBuddy byteBuddy = new ByteBuddy();
+		TypePool typePool = TypePool.Default.of(pachedClassLoader);
+		final ClassFileLocator classFileLocator = ClassFileLocator.ForClassLoader.of(pachedClassLoader);
+
+		final MethodDelegation cachedFieldInterceptor = MethodDelegation.to(CachedFieldInterceptor.class);
+		byteBuddy.rebase(typePool.describe(CACHED_FIELD_CLASS).resolve(), //
+				classFileLocator)
+		.method(ElementMatchers.named("getProperty")).intercept(cachedFieldInterceptor)
+		.method(ElementMatchers.named("setProperty")).intercept(cachedFieldInterceptor)
+		.make()
+		.load(pachedClassLoader);
+
+		final MethodDelegation cachedMethodInterceptor = MethodDelegation.to(CachedMethodInterceptor.class);
+		final MethodDelegation cachedMethodInvocationInterceptor = MethodDelegation.to(CachedMethodInvocationInterceptor.class);
+
+		byteBuddy.rebase(typePool.describe(CACHED_METHOD_CLASS).resolve(), //
+				classFileLocator)
+		.method(ElementMatchers.named("setAccessible")).intercept(cachedMethodInterceptor)
+		.method(ElementMatchers.named("getCachedMethod")).intercept(cachedMethodInterceptor)
+		.method(ElementMatchers.named("invoke")).intercept(cachedMethodInvocationInterceptor)
+		.make()
+		.load(pachedClassLoader);
+	}
+}
