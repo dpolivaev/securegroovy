0940b24349c5ae665c004a6cc3911b2d6ce0e965
Dimitry Polivaev <dpolivaev@gmx.de>
2017-04-08 14:23:28 +0200

    Fix access permission checks

--- src/main/java/org/freeplane/securegroovy/AccessPermissionChecker.java
+++ src/main/java/org/freeplane/securegroovy/AccessPermissionChecker.java
@@ -12,28 +12,34 @@ class AccessPermissionChecker {
 
 	private static final ReflectPermission REFLECT_PERMISSION = new ReflectPermission("suppressAccessChecks");
 
-	static private void checkAccessPermission(Class<?> declaringClass, final int modifiers, boolean isAccessible, String memberType,
-	                                  String name) {
+	static void checkAccessPermission(Class<?> declaringClass, final int modifiers, boolean isAccessible, String memberType,
+			String name) throws IllegalArgumentException {
 		final SecurityManager securityManager = System.getSecurityManager();
-		if (isAccessible && securityManager != null && (modifiers & (Modifier.PUBLIC | Modifier.PROTECTED)) == 0
-		        && !GroovyObject.class.isAssignableFrom(declaringClass)) {
+		if (isAccessible && securityManager != null) {
 			try {
-				securityManager.checkPermission(REFLECT_PERMISSION);
+				if ((modifiers & (Modifier.PUBLIC | Modifier.PROTECTED)) == 0
+						&& !GroovyObject.class.isAssignableFrom(declaringClass)) {
+					securityManager.checkPermission(REFLECT_PERMISSION);
+				}
+				else if ((modifiers & (Modifier.PUBLIC)) == 0
+						&& declaringClass.equals(ClassLoader.class)){
+					securityManager.checkCreateClassLoader();
+				}
 			}
-			catch (AccessControlException ex){
+			catch (AccessControlException ex) {
 				throw new IllegalArgumentException("Illegal access to " + memberType + " " + name);
 			}
 		}
 	}
 
-	static public void checkAccessPermission(Method method) {
+	static public void checkAccessPermission(Method method)  throws IllegalArgumentException {
 		checkAccessPermission(method.getDeclaringClass(), method.getModifiers(), method.isAccessible(), "method",
 				method.getName());
 	}
 
-	public static void checkAccessPermission(Field field) {
+	public static void checkAccessPermission(Field field)  throws IllegalArgumentException  {
 		checkAccessPermission(field.getDeclaringClass(), field.getModifiers(), field.isAccessible(),
-		    "field", field.getName());
+				"field", field.getName());
 	}
 
 }
