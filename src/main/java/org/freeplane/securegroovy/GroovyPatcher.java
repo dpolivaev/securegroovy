3f21061712ed0a371fcea90963b472d7d3887c3b
Dimitry Polivaev <dpolivaev@gmx.de>
2017-04-13 18:02:51 +0200

    change API

--- src/main/java/org/freeplane/securegroovy/GroovyPatcher.java
+++ src/main/java/org/freeplane/securegroovy/GroovyPatcher.java
@@ -1,6 +1,5 @@
 package org.freeplane.securegroovy;
 
-import groovy.lang.GroovyObject;
 import net.bytebuddy.ByteBuddy;
 import net.bytebuddy.dynamic.ClassFileLocator;
 import net.bytebuddy.dynamic.loading.ClassLoadingStrategy;
@@ -13,13 +12,14 @@ public class GroovyPatcher {
 	private static final String CACHED_FIELD_CLASS = "org.codehaus.groovy.reflection.CachedField";
 	private static final String CACHED_METHOD_CLASS = "org.codehaus.groovy.reflection.CachedMethod";
 	
-	public static void apply(final ClassLoader pachedClassLoader){
+	public static void apply(Class<?> groovyClass){
+		final ClassLoader pachedClassLoader = groovyClass.getClassLoader();
 		final ByteBuddy byteBuddy = new ByteBuddy();
 		TypePool typePool = TypePool.Default.of(pachedClassLoader);
 		final ClassFileLocator classFileLocator = ClassFileLocator.ForClassLoader.of(pachedClassLoader);
 
 		final MethodDelegation cachedFieldInterceptor = MethodDelegation.to(CachedFieldInterceptor.class);
-		final Configurable<ClassLoader> strategy = ClassLoadingStrategy.Default.INJECTION.with(GroovyObject.class.getProtectionDomain());
+		final Configurable<ClassLoader> strategy = ClassLoadingStrategy.Default.INJECTION.with(groovyClass.getProtectionDomain());
 		byteBuddy.rebase(typePool.describe(CACHED_FIELD_CLASS).resolve(), //
 				classFileLocator)
 		.method(ElementMatchers.named("getProperty")).intercept(cachedFieldInterceptor)
